cb6a0382400ccb815aa5666704352e5c26946c65
rosnbro <benro95@gmail.com>
2022-11-28 21:14:02 -0500

    simplify geocode and weather data functions, add simple form and listeners to trigger api call

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+const locationInput = document.getElementById('location');
+const locationButton = document.getElementById('locationButton');
+
+locationButton.addEventListener('click', () => search());
+locationInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    search();
+    console.log(e)
+    e.preventDefault();
+  }
+});
+
 async function weatherData(location, units) {
   try {
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=53a8ca4edc6d9f89d1a3c6144858865a&units=${units}`, {mode: 'cors'});
     let weather = await response.json();
-    let data = {
+    return {
       feels_like: weather['main']['feels_like'],
       humidity: weather['main']['humidity'],
       pressure: weather['main']['pressure'],
@@ -10,12 +22,11 @@ async function weatherData(location, units) {
       name: weather['name'],
       sunrise: weather['sys']['sunrise'],
       sunset: weather['sys']['sunset'],
-      description: weather['weather']['description'],
+      description: weather['weather'][0]['description'],
       windspeed: weather['wind']['speed'],
       windDir: weather['wind']['deg'],
       units: units,
-    }
-    return data;
+    };
   } catch (err) {
     console.log(err);
   }
@@ -23,31 +34,21 @@ async function weatherData(location, units) {
 
 async function geocodeData(city, country, state) {
   try {
+    let url = '';
     if (state) {
-      let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=1&appid=53a8ca4edc6d9f89d1a3c6144858865a`, {mode: 'cors'});
-      let location = await response.json();
-      let coordinates = {
-        lat: location[0]['lat'],
-        lon: location[0]['lon'],
-      }
-      return coordinates;
+      url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=1&appid=53a8ca4edc6d9f89d1a3c6144858865a`;
     } else if (country) {
-      let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=53a8ca4edc6d9f89d1a3c6144858865a`, {mode: 'cors'});
-      let location = await response.json();
-      let coordinates = {
-        lat: location[0]['lat'],
-        lon: location[0]['lon'],
-      }
-      return coordinates;
+      url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=53a8ca4edc6d9f89d1a3c6144858865a`;
     } else {
-      let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=53a8ca4edc6d9f89d1a3c6144858865a`, {mode: 'cors'});
-      let location = await response.json();
-      let coordinates = {
-        lat: location[0]['lat'],
-        lon: location[0]['lon'],
-      }
-      return coordinates;
+      url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=53a8ca4edc6d9f89d1a3c6144858865a`;
     }
+
+    let response = await fetch(url, {mode: 'cors'});
+    let location = await response.json();
+    return {
+      lat: location[0]['lat'],
+      lon: location[0]['lon'],
+    };
   } catch (err) {
     console.log(err);
   }
